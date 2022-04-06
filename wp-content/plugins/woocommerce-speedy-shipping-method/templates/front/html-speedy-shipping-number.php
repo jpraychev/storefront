92227aa87fd89ea33ad7e0d05cf4bd7def1d7dfa
Jordan Raychev <jpraychev@gmail.com>
2022-04-06 06:40:35 +0300

    wp-content folder removed

--- wp-content/plugins/woocommerce-speedy-shipping-method/templates/front/html-speedy-shipping-number.php
+++
@@ -1,6 +0,0 @@
-<?php if($bol_id) { ?>
-	<tr>
-		<th><?php _e( 'Номер на товарителница:', SPEEDY_TEXT_DOMAIN ); ?></th>
-		<td><a href="https://www.speedy.bg/bg/track-shipment/?shipmentNumber=<?php echo $bol_id; ?>" target="_blank"><?php echo $bol_id; ?></a></td>
-	</tr>
-<?php } ?>
